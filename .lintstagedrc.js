2ca50a4336322f91862ff3a9044520b1adc6d583
Yoon Kim <yoonkim@Yoons-MacBook-Pro.local>
2023-03-29 16:41:25 -0700

    deleted lintstaged

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '**/*.(ts|tsx)': 'eslint --fix',
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `eslint ${filenames.join(' ')}`,
-    `prettier --write ${filenames.join(' ')}`,
-  ],
-  '**/*.(md|json)': (filenames) =>
-  `prettier --write ${filenames.join(' ')}`,
-}
